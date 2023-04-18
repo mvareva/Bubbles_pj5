dff21a8362128e135562bb55cf187b6e67e2d325
mvareva <114020219+mvareva@users.noreply.github.com>
2023-04-18 19:29:05 -0400

    updates

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let imgb = document.getElementsByTagName("img");
-let audio = new Audio ("pop.wav");
+let audio = new Audio ("Pop-1.mov");
 console.log(imgb.length);
 for (let i = 0; i <imgb.length; i++) {
 imgb[i].addEventListener("click", (event)=>{
-imgb[i].setAttribute("src", "07 2.png");  
+imgb[i].setAttribute("src", "popped.png");  
+imgb[i].classList.add('popped')
 audio.play();
 }); 
 }
